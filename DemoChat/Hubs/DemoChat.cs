9f458d97005b14daab1aba749041a4f0968ced32
Huỳnh <huynh.it.24@gmail.com>
2022-02-20 15:52:30 +0700

    Add project files.

---
+++ DemoChat/Hubs/DemoChat.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoChat.Hubs
+{
+    [HubName("chat")]
+    public class DemoChat : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void Connect(string name)
+        {
+            Clients.Caller.name(name);
+        }
+        public void Message(string name, string message)
+        {
+            Clients.All.message(name,message);
+        }
+    }
+}
